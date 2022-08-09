be3b13c706e6a9ff6f2969418931d3442138de65
Flad <64989593+Flad0@users.noreply.github.com>
2022-08-08 19:41:55 -0700

    Add trustpermissions struct

---
+++ src/main/java/com/opencratesoftware/mcnotepad/structs/TrustPermissions.java
@@ -0,0 +1,8 @@
+package com.opencratesoftware.mcnotepad.structs;
+
+public class TrustPermissions 
+{
+    public boolean read = false;
+    public boolean write = false;
+    public boolean remove = false;    
+}
