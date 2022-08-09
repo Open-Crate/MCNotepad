3e6f0e162d170b99df54cda05cb0e87fa0881033
Flad <64989593+Flad0@users.noreply.github.com>
2022-08-09 16:34:57 -0700

    remove 'remove' permission from trustpermissions

--- src/main/java/com/opencratesoftware/mcnotepad/structs/TrustPermissions.java
+++ src/main/java/com/opencratesoftware/mcnotepad/structs/TrustPermissions.java
@@ -3,6 +3,5 @@ package com.opencratesoftware.mcnotepad.structs;
 public class TrustPermissions 
 {
     public boolean read = false;
-    public boolean write = false;
-    public boolean remove = false;    
+    public boolean write = false; 
 }
